eca90e3e514f80618bb6b0387cc17c95af73c9ea
Daniel Schruhl <danielschruhl@gmail.com>
2020-03-20 16:25:08 +0100

    Shuffle balanced datasets

--- src/main/scala/com/github/discoverai/tabula/Tabula.scala
+++ src/main/scala/com/github/discoverai/tabula/Tabula.scala
@@ -2,6 +2,7 @@ package com.github.discoverai.tabula
 
 import java.io.PrintWriter
 import java.nio.file.{Files, Paths}
+
 import scala.util.Random
 
 object Tabula {
@@ -19,17 +20,25 @@ object Tabula {
       case Array(inputFile, outputFolder) =>
         try {
           println(s"Start writing files for '$inputFile' to folder '$outputFolder'")
-          val intentInputs: Seq[IntentInputData] = Random.shuffle(Parser.parseMdFile(inputFile))
+          val intentInputs: Seq[IntentInputData] = Parser.parseMdFile(inputFile)
           val labels = DatamartHelper.labels(intentInputs)
           val labelFile = Paths.get(outputFolder + "/labels.csv")
           val labelWriter = new PrintWriter(Files.newOutputStream(labelFile))
           labels.foreach(label => labelWriter.append(s"${label._2},${label._1}\n"))
           labelWriter.close()
 
-          val trainAndTestData = intentInputs.splitAt((intentInputs.length * 0.8).toInt)
+          val (trainDataset, testDataset) = Parser.splitDataset(0.8, intentInputs)
 
-          writeDatamart(outputFolder + "/train.csv", trainAndTestData._1, labels)
-          writeDatamart(outputFolder + "/test.csv", trainAndTestData._2, labels)
+          writeDatamart(
+            outputFolder + "/train.csv",
+            Random.shuffle(trainDataset),
+            labels
+          )
+          writeDatamart(
+            outputFolder + "/test.csv",
+            Random.shuffle(testDataset),
+            labels
+          )
 
           val headers = "label,sentence"
           val headersFile = Paths.get(outputFolder + "/headers.csv")
